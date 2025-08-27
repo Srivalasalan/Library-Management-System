8afc8dc616f102931bc60d9e7ac24052476d869c
Srivalasalan <144202207+Srivalasalan@users.noreply.github.com>
2025-08-27 19:40:54 +0530

    fix: added constructor in Book.java

--- Book.java
+++ Book.java
@@ -3,4 +3,13 @@ public class Book {
     String title;
     String author;
     int quantity;
+    Member m;
+
+    Book( int bookId,String title,String author,int quantity,Member m){
+        this.bookId=bookId;
+        this.title=title;
+        this.author=author;
+        this.quantity=quantity;
+        this.m=m;
+    }
 }
