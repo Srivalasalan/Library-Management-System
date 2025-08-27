0038ec87d13db95ba0d057c931db29d423a52cb1
Srivalasalan <144202207+Srivalasalan@users.noreply.github.com>
2025-08-27 19:38:23 +0530

    fix: added constructor Member.java

--- Member.java
+++ Member.java
@@ -2,5 +2,11 @@ public class Member {
     int memberId;
     String name;
     String contactInfo;
+
+    Member(int memberId,String name,String contactInfo){
+        this.memberId=memberId;
+        this.name=name;
+        this.contactInfo=contactInfo;
+    }
     
 }
