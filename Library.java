6e21064c6760c2e26e12a5e4749953f37cc43861
Srivalasalan <144202207+Srivalasalan@users.noreply.github.com>
2025-08-31 13:10:31 +0530

    fix: Simplify addBook method signature by removing unnecessary parameters

--- Library.java
+++ Library.java
@@ -1,6 +1,6 @@
 public interface Library {
 
-    void addBook(Book b, int id, String pass,Admin a);
+    void addBook(Admin a);
     void addMember(Member m, int id);
     void issueBook(int bookId,int memberId);
     void returnBook(int bookId,int memberId);
