125c63c2387f6a084f5bbaf98ee9dadf6297bc4b
Srivalasalan <144202207+Srivalasalan@users.noreply.github.com>
2025-09-13 19:43:02 +0530

    feat: Add Transaction class to manage book transactions with properties and constructor

---
+++ Transaction.java
@@ -0,0 +1,22 @@
+package pro1;
+
+import java.util.Date;
+
+public class Transaction {
+
+	int transactionId;
+	Date date;
+	Book b;
+	Member m;
+	
+	public Transaction(int transactionId, Book b, Member m) {
+		
+		this.transactionId = transactionId;
+		this.b = b;
+		this.m = m;
+		this.date = new Date();
+		
+	}
+	
+	
+}
