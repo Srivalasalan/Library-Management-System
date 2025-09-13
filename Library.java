05b735bad2498701d6739dcbeb33de7e7eade28c
Srivalasalan <144202207+Srivalasalan@users.noreply.github.com>
2025-09-13 19:41:37 +0530

    refactor: Update Library class structure and methods for book management

--- Library.java
+++ Library.java
@@ -1,11 +1,40 @@
-public interface Library {
+package pro1;
 
-    void addBook(Book b, int id, String pass,Admin a);
-    void addMember(Member m, int id);
-    void issueBook(int bookId,int memberId);
-    void returnBook(int bookId,int memberId);
-    void searchBook(String title);
-    void displayBooks();
-    void displayMembers();
-    
+
+import java.util.LinkedList;
+
+public class Library {
+
+	LinkedList<Book> books = new LinkedList<Book>();
+	LinkedList<Member> members = new LinkedList<Member>();
+	
+
+	
+
+	public void searchBook(String title) {
+		for(Book b: books) {
+			if(b.title.equalsIgnoreCase(title)) {
+				System.out.println("Book Found! ");
+				System.out.println("Book Name: "+ b.title +" Author: "+ b.author);
+			}
+			else {
+				System.out.println("Book Not Found");
+			}
+		}
+	}
+
+	public void getAvailableBook() {
+
+		for(Book b : books) {
+			if(!b.issued) {
+				System.out.println("Available Books: \n"+b.title);
+			}
+		}
+	}
+
+	public void addBook(Book book) {
+ 		books.add(book);
+		System.out.println(book.title+" Book added");
+
+	}
 }
