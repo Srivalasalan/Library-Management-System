cea77347432bd419b42053d7f8fa639b39501f3e
Srivalasalan <144202207+Srivalasalan@users.noreply.github.com>
2025-09-13 19:42:47 +0530

    refactor: Revise Member class to use LinkedList for book management and update constructor parameters

--- Member.java
+++ Member.java
@@ -1,15 +1,47 @@
-import java.util.ArrayList;
+package pro1;
+
+import java.util.LinkedList;
 
 public class Member {
-    ArrayList<Member> mem=new ArrayList<>();
-    int memberId;
-    String name;
-    String contactInfo;
 
-    Member(int memberId,String name,String contactInfo){
-        this.memberId=memberId;
-        this.name=name;
-        this.contactInfo=contactInfo;
-    }
-    
+	int memberId;
+	String name;
+	long contact;
+	LinkedList<Book> books = new LinkedList<Book>();
+
+	public Member(int memberId, String name, long contact, Book b) {
+
+		this.memberId = memberId;
+		this.name = name;
+		this.contact = contact;
+	}
+
+	public void borrowBook(Book book) {
+
+		if (!book.issued) {
+			book.issue();
+			books.add(book);
+			System.out.println(name + " Borrowed: " + book.title);
+		} else {
+			System.out.println("Book Not Available");
+		}
+
+	}
+
+	public void returnBook(Book book) {
+
+		if (books.remove(book)) {
+			book.returnBook();
+			System.out.println(name + "has Returned " + book.title);
+
+		} else {
+			System.out.println("You don't buy this book");
+		}
+
+	}
+
+	public void reserveBook(Book book) {
+		book.reserve();
+	}
+
 }
