79259b1c01a0a859b3a5b26fb2a373367ec5e30e
Srivalasalan <144202207+Srivalasalan@users.noreply.github.com>
2025-09-13 19:41:24 +0530

    feat: Implement Librarian class with methods for managing books

---
+++ Librarian.java
@@ -0,0 +1,40 @@
+package pro1;
+
+public class Librarian {
+
+	int librarianId;
+	String name;
+
+	public Librarian(int librarianId, String name) {
+
+		this.librarianId = librarianId;
+		this.name = name;
+
+	}
+
+	public void addBook(Library lib, Book book) {
+
+		lib.addBook(book);
+		System.out.println("Book added added by " + name);
+
+	}
+
+	public void removeBook(Library lib, Book book) {
+		if (lib.books.remove(book)) {
+			System.out.println("Book Removed By " + name);
+		} else {
+			System.out.println("Book not Found");
+		}
+
+	}
+
+	public void issueBook(Book book, Member mem) {
+		mem.borrowBook(book);
+
+	}
+
+	public void returnBook(Book book, Member mem) {
+		mem.reserveBook(book);
+	}
+
+}
