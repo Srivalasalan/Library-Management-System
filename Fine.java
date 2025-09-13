4c203f723f3bfd502822be6ab9c540062dcfd25a
Srivalasalan <144202207+Srivalasalan@users.noreply.github.com>
2025-09-13 19:41:14 +0530

    feat: Create Fine class to manage library fines with properties and methods

---
+++ Fine.java
@@ -0,0 +1,31 @@
+package pro1;
+
+import java.util.Date;
+
+public class Fine {
+	
+	int fineId;
+	double amount;
+	Date dueDate;
+	
+	
+	public Fine(int fineId, double amount,Date dueDate) {
+		this.fineId = fineId;
+		this.amount = amount;
+		this.dueDate = new Date();
+		
+		
+	}
+	
+	
+	public void generateFine() {
+		
+	}
+	
+	public void payFine() {
+		
+	}
+	
+	
+
+}
