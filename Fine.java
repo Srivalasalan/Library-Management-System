0195f911b2d9db50032e249b31078683ac7f83f2
Srivalasalan <144202207+Srivalasalan@users.noreply.github.com>
2025-09-15 01:58:15 +0530

    refactor: Update Fine class to improve fine calculation and payment methods

--- Fine.java
+++ Fine.java
@@ -1,5 +1,6 @@
 
-
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.Date;
 
 public class Fine {
@@ -12,17 +13,31 @@ public class Fine {
 	public Fine(int fineId, double amount,Date dueDate) {
 		this.fineId = fineId;
 		this.amount = amount;
-		this.dueDate = new Date();
+		this.dueDate = dueDate;
 		
 		
 	}
 	
 	
-	public void generateFine() {
+	public void generateFine(LocalDate returnDate,Book book) {
+		if(returnDate.isAfter(book.dueDate)) {
+			long daysLate=ChronoUnit.DAYS.between(book.dueDate, returnDate);
+			amount =daysLate*amount;
+			System.out.println("Fine Amount: "+amount);
+		}
 		
 	}
 	
-	public void payFine() {
+	public void payFine(double amount) {
+		
+		if(amount>0&&amount==this.amount) {
+			System.out.println("Fine Paid Successfully!");
+			amount=0;
+		}
+		else {
+			System.out.println("Fine Not Paid");
+			
+		}
 		
 	}
 	
