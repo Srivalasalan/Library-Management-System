28080acfa028d8cc614626bc0467b6422920eae1
Srivalasalan <144202207+Srivalasalan@users.noreply.github.com>
2025-09-15 01:57:43 +0530

    refactor: Clean up whitespace and formatting in Administrator class

--- Administrator.java
+++ Administrator.java
@@ -1,15 +1,34 @@
-
 public class Administrator {
-	
+
 	int adminId;
 	String name;
-	
+
 	public Administrator(int adminId, String name) {
 		this.adminId = adminId;
 		this.name = name;
-		
+
 	}
-	
-	
+
+	public void addMember(Library libr, Member mem) {
+		libr.members.add(mem);
+		System.out.println("Member added by: " + name + "(admin)");
+
+	}
+
+	public void removeMember(Library libr, Member mem) {
+		if (libr.members.remove(mem)) {
+			System.out.println(mem.name + " Removed by: " + name + "(admin)");
+		} else {
+			System.out.println("Member Not Found");
+		}
+	}
+
+	public void viewReports(Library libr) {
+
+		System.out.println("Library Report: ");
+		System.out.println("Number of Books "+libr.books.size());
+		System.out.println("Number of Members "+libr.members.size());
+
+	}
 
 }
