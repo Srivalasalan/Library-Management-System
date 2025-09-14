dc2fad1df579fc3092a2f4adffcfd4ce5158438d
Srivalasalan <144202207+Srivalasalan@users.noreply.github.com>
2025-09-15 01:59:05 +0530

    refactor: Replace Date with LocalDate in Transaction class and clean up code

--- Transaction.java
+++ Transaction.java
@@ -1,22 +1,39 @@
+import java.time.LocalDate;
 
-
-import java.util.Date;
 
 public class Transaction {
 
 	int transactionId;
-	Date date;
+	LocalDate date;
 	Book b;
 	Member m;
-	
+
 	public Transaction(int transactionId, Book b, Member m) {
-		
+
 		this.transactionId = transactionId;
 		this.b = b;
 		this.m = m;
-		this.date = new Date();
-		
+		this.date = LocalDate.now();
+
 	}
-	
-	
+
+	public void create() {
+		
+		System.out.println("The Transaction is created for the Book"+b.title + " and for the Member: "+m.name);
+		System.out.println("Date: "+ date);
+	}
+
+	public void update(Book newBook) {
+		this.b=newBook;
+		System.out.println("The new Book is updated , Book Name: "+newBook);
+
+	}
+
+	public void delete() {
+		System.out.println("Transaction has been Deleted for Book: "+b.title+ " and for "+m.name);
+		this.b=null;
+		this.m=null;
+
+	}
+
 }
